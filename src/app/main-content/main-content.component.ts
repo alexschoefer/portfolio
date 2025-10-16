d32a69741365060aeae417266f80b4347891a011
alexschoefer <alex.schoefer@gmx.net>
2025-10-16 21:59:29 +0200

    feat: add new component about me; first steps of structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { MarqueeComponent } from './landingPage/marquee/marquee.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, NavbarComponent, MarqueeComponent],
+  imports: [CommonModule, LandingPageComponent, NavbarComponent, MarqueeComponent, AboutComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
