ca68e0fbba312b0e6e135292e494e935f53aca4e
alexschoefer <alex.schoefer@gmx.net>
2025-10-20 20:39:58 +0200

    feat: working on the skills component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { LandingPageComponent } from './landingPage/landingPage.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { MarqueeComponent } from './landingPage/marquee/marquee.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, NavbarComponent, MarqueeComponent, AboutComponent],
+  imports: [CommonModule, LandingPageComponent, NavbarComponent, MarqueeComponent, AboutComponent,SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
