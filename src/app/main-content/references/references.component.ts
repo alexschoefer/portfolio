3852d7778b0fb461c79a63ba7b877b3c0837e767
alexschoefer <alex.schoefer@gmx.net>
2025-10-28 20:56:13 +0100

    feat: add functions to switch through the references; hover effect for the arrows

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
+  comments = [
+    { text: 'Comment left', author: 'Author 1' },
+    { text: 'Lukas has proven to be a reliable group partner...', author: 'H. Janisch - Team Partner' },
+    { text: 'Comment right', author: 'Author 3' }
+  ];
 
+  currentActiveComment = 1; 
+  boxWidth = 632 + 32;     
+  startIndex = 1;       
+
+  nextComment() {
+    this.currentActiveComment = (this.currentActiveComment + 1) % this.comments.length;
+  }
+
+  preComment() {
+    this.currentActiveComment = (this.currentActiveComment - 1 + this.comments.length) % this.comments.length;
+  }
+
+  isCurrent(index: number) {
+    return index === this.currentActiveComment;
+  }
+
+  getTranslateX() {
+    return -(this.currentActiveComment - this.startIndex) * this.boxWidth;
+  }
 }
