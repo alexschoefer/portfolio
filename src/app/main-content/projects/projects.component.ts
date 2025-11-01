62a85d38514c51f37e0e63b31c6feea1e252fd2f
alexschoefer <alex.schoefer@gmx.net>
2025-11-01 17:16:29 +0100

    feat: add function to open the project details

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,13 +1,74 @@
 import { Component } from '@angular/core';
 import { ProjectDetailComponent } from '../../project-detail/project-detail.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ProjectDetailComponent],
+  imports: [ProjectDetailComponent, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  selectedProject: any = null;
+
+  openProject(project: any) {
+    this.selectedProject = project;
+  }
+
+  closeProject() {
+    this.selectedProject = null;
+  }
+
+  projects = [
+    {
+      projectID: '01',
+      projectName: 'Join',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      technologies: [
+        { name: 'HTML', icon: '../../../assets/icons/html-icon.png' },
+        { name: 'CSS', icon: '../../../assets/icons/css-icon.png' },
+        { name: 'JavaScript', icon: '../../../assets/icons/javascript-icon.png' },
+        { name: 'Firebase', icon: '../../../assets/icons/firebase-icon.png' },
+      ],
+      image: '../../../assets/img/join_mainscreen.png',
+      imageOverlay: [
+        '../../../assets/img/join_mainscreen.png'
+      ],
+      gitHublink: 'https://join.example.com'
+    },
+    {
+      projectID: '02',
+      projectName: 'El Pollo Loco',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      technologies: [
+        { name: 'HTML', icon: '../../../assets/icons/html-icon.png' },
+        { name: 'CSS', icon: '../../../assets/icons/css-icon.png' },
+        { name: 'JavaScript', icon: '../../../assets/icons/javascript-icon.png' },
+      ],
+      image: '../../../assets/img/el_pollo_loco_mainscreen.png',
+      imageOverlay: [
+        '../../../assets/img/project-screenshot-el_pollo_overlay.png'
+      ],
+      gitHublink: 'https://join.example.com'
+    },
+    {
+      projectID: '03',
+      projectName: 'Pokedex',
+      description: 'This interactive Pokédex, built with HTML, CSS, and JavaScript, pulls data from an API to display each Pokémon’s name, image, and type.',
+      technologies: [
+        { name: 'HTML', icon: '../../../assets/icons/html-icon.png' },
+        { name: 'CSS', icon: '../../../assets/icons/css-icon.png' },
+        { name: 'JavaScript', icon: '../../../assets/icons/javascript-icon.png' },
+      ],
+      image: '../../../assets/img/pokedex_mainscreen.png',
+      imageOverlay: [
+        '../../../assets/img/pokedex_mainscreen.png'
+      ],
+      gitHublink: 'https://join.example.com'
+    }
+  ]
+
 
 }
--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,32 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss'
 })
 export class ProjectDetailComponent {
-  projects = [
-    {
-      projectID: '01',
-      name: 'Join',
-      technologies: 'HTML | CSS | JavaScript | Firebase',
-      image: '../../../assets/img/join_mainscreen.png',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
-    },
-    {
-      name: 'El Pollo Loco',
-      technologies: 'HTML | CSS | JavaScript',
-      image: '../../../assets/img/el_pollo_logo_mainscreen.png',
-      description: 'Beschreibung für El Pollo Loco Projekt'
-    },
-    {
-      name: 'Pokedex',
-      technologies: 'HTML | CSS | JavaScript',
-      image: '../../../assets/img/pokedex_mainscreen.png',
-      description: 'Beschreibung für Pokedex Projekt'
-    }
-  ];
+  @Input() project: any;     
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
 }
