61eae968ddd8c0aa04af460a22e798d8c4325ee9
alexschoefer <alex.schoefer@gmx.net>
2025-11-01 22:23:18 +0100

    feat: add first steps for switching language in the navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isEnglish: boolean = true;
 
+  toggleLanguage(lang?: 'EN' | 'DE') {
+    if (lang) {
+      this.isEnglish = lang === 'EN';
+    } else {
+      this.isEnglish = !this.isEnglish;
+    }
+  }
 }
