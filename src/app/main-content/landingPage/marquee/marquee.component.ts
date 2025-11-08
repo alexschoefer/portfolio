fe05bc53450f9043e2bb2274f037b915076cd2ff
alexschoefer <alex.schoefer@gmx.net>
2025-11-08 23:27:46 +0100

    fix: bugfixes and changes in media queries over all components part 1

--- src/app/main-content/landingPage/marquee/marquee.component.ts
+++ src/app/main-content/landingPage/marquee/marquee.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-marquee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ],
   templateUrl: './marquee.component.html',
   styleUrl: './marquee.component.scss'
 })
 export class MarqueeComponent {
-
+  
 }
