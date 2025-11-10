458bbc6a0cded041fe474e867b4e4e5f05e38376
alexschoefer <alex.schoefer@gmx.net>
2025-11-10 22:58:36 +0100

    fix: bugfixes and changes in media queries in some components

--- src/app/main-content/landingPage/landingPage.component.ts
+++ src/app/main-content/landingPage/landingPage.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { MarqueeComponent } from './marquee/marquee.component';
-import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule, NavbarComponent, MarqueeComponent, TranslateModule],
+    imports: [CommonModule, NavbarComponent, MarqueeComponent],
     templateUrl: './landingPage.component.html', 
     styleUrls: ['./landingPage.component.scss']
 })
+
 
 export class LandingPageComponent {
 }
