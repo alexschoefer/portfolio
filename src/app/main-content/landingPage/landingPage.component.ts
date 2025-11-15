b5772046471691f6ee719995e2c2031432cc39d9
alexschoefer <alex.schoefer@gmx.net>
2025-11-15 22:43:06 +0100

    fix: bugfixes in media queries; add translation of the start buttons contact and work

--- src/app/main-content/landingPage/landingPage.component.ts
+++ src/app/main-content/landingPage/landingPage.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { MarqueeComponent } from './marquee/marquee.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule, NavbarComponent, MarqueeComponent],
+    imports: [CommonModule, NavbarComponent, MarqueeComponent, TranslateModule],
     templateUrl: './landingPage.component.html', 
     styleUrls: ['./landingPage.component.scss']
 })
